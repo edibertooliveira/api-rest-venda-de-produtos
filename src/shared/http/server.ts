6e26ae900481e24856d9a68e1e940d25af26d111
Ediberto Oliveira <edibertooliveira@aol.com>
2021-06-19 23:19:42 -0300

    addd express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello Dev!');
+import env from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+env.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors);
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`port: ${PORT}`);
+});
