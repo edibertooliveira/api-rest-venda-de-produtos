2503bb670ec7108c5c015602addee22548216bf1
Ediberto Oliveira <edibertooliveira@aol.com>
2021-06-20 19:51:55 -0300

    instalando modulo celebrate e aplicando as rotas

--- src/modules/produts/routers/product.routes.ts
+++ src/modules/produts/routers/product.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import ProductsController from '../controller/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productRouter = Router();
 const productsController = new ProductsController();
@@ -7,12 +8,48 @@ const productsController = new ProductsController();
 productRouter
   .route('/')
   .get(productsController.index)
-  .post(productsController.create);
+  .post(
+    celebrate({
+      [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().required(),
+        quantity: Joi.number().required(),
+      },
+    }),
+    productsController.create,
+  );
 
-productRouter
-  .route('/:id')
-  .get(productsController.show)
-  .put(productsController.update)
-  .delete(productsController.delete);
+productRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.show,
+);
+productRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.update,
+);
+productRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.delete,
+);
 
 export default productRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import routes from './routers';
 import AppError from '../errors/ApiError';
 import '../typeorm';
+import { errors } from 'celebrate';
 env.config();
 
 const app = express();
@@ -13,6 +14,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof AppError) {
