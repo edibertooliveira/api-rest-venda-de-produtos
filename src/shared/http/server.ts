1af9ef2574e1f79b0f7b25017c64ab48759999e9
Ediberto Oliveira <edibertooliveira@aol.com>
2021-06-20 10:45:47 -0300

    tratamento de errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,13 +1,30 @@
 import env from 'dotenv';
-import express from 'express';
+import AppError from '../errors/ApiError';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 env.config();
+
+import router from './routers/';
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors);
 app.use(express.json());
+app.use('/api', router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`port: ${PORT}`);
