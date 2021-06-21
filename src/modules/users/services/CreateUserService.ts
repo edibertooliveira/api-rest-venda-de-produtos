2c35887d2f5d8b59a8f5464a1d8b8afc5dd53d3d
Ediberto Oliveira <edibertooliveira@aol.com>
2021-06-20 22:25:40 -0300

    adicionado hash ao criar users

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import AppError from '../../../shared/errors/ApiError';
 import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import { UserRepository } from '../typeorm/repositories/UsersRepository';
 import User from '../typeorm/entities/User';
 interface IRequest {
@@ -12,7 +13,7 @@ export default class CreateUserService {
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const userRepository = getCustomRepository(UserRepository);
     const userExists = await userRepository.findByEmail(email);
-
+    const hashedPassword = await hash(password, 8);
     if (userExists) {
       throw new AppError('There is already one user with this email', 400);
     }
@@ -20,7 +21,7 @@ export default class CreateUserService {
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
