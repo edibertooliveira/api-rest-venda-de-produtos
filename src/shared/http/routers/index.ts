d606f5576ec29f1f38329ec5503aeacf6e77e61a
Ediberto Oliveira <edibertooliveira@aol.com>
2021-08-10 10:21:21 -0300

    adicionado serviços crud

--- src/shared/http/routers/index.ts
+++ src/shared/http/routers/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import productRouter from '../../../modules/products/routers/product.routes';
-import userRouter from '../../../modules/users/routers/user.routes';
-import sessionRouter from '../../../modules/users/routers/session.routes';
+import productRouter from '@modules/products/routers/product.routes';
+import userRouter from '@modules/users/routers/user.routes';
+import sessionRouter from '@modules/users/routers/session.routes';
+import passwordRouter from '@modules/users/routers/password.routes';
 
 const routes = Router();
 
 routes.use('/login', sessionRouter);
 routes.use('/products', productRouter);
 routes.use('/users', userRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
